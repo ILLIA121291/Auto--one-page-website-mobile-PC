95c7274afba244169e78dc26a06ef25242aa2c86
ILLIA121291 <illia121291@gmail.com>
2023-12-09 14:16:01 +0300

    Section 7: F;  Section 8: F;

--- auto_main.js
+++ auto_main.js
@@ -18,9 +18,10 @@ $(function () {
         infinity: true,
         draggable:false,
         waitForAnimate: false,
+        appendDots:$('.our_customers_block_two_buttons_indicator'),
         autoplay: true,
         autoplaySpeed: 2000,
-        
+        dots: true,
         
       }) 
       $('.our_customers_slider_button_previous').on('click', function (e) {
@@ -42,17 +43,6 @@ $(function () {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 })
 
 
@@ -64,19 +54,43 @@ for (let card of articleCards) {
     let titelColor = document.querySelectorAll('.hover_js')[card.getAttribute('data-order')-1]
 
     card.onmouseenter = function(event){
-        titelColor.style.color = '#61C3A1'
+      titelColor.style.color = '#61C3A1'
+      card.style.background = '#eaedef'
     }
 
     card.onmouseleave = function(event){
     titelColor.style.color = ''
+    card.style.background = ''
     }
 
     card.onmousedown = function(event){    
     titelColor.style.color = ''
+    card.style.background = ''
 }
 }
 // Section - 5 - "Our blog" - Titel color - End -->
 
 
+// Section - 7 - "Frequently" - Start -->
+
+let frequentlyBtnAll = document.querySelectorAll('.frequently_section_header_button')
+let b = document.querySelector('.frequently_section_text')
+let c = document.querySelector('.frequently_section_button_close')
 
 
+for (let btn of frequentlyBtnAll) {
+
+  btn.addEventListener ('click', openSection)
+  
+  
+  function openSection (event){
+    let text = document.querySelectorAll('.frequently_section_text')[btn.getAttribute('data-number')-1]
+    let indicator = document.querySelectorAll('.frequently_section_button_close')[btn.getAttribute('data-number')-1]
+    text.hidden = !text.hidden
+    indicator.classList.toggle('frequently_section_close')
+  }
+
+}
+
+
+// Section - 7 - "Frequently" - End -->
